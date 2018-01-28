3983c6294ecdecb82466377a14236f50a0befaf3
GodBlessToNo1 <fengliuzhuan0329@126.com>
2018-01-28 22:18:20 +0800

    duck_typing
    多态

---
+++ duck_typig.py
@@ -0,0 +1,22 @@
+class Parrot:
+    def fly(self):
+        print("Parrot flying")
+
+class Airplane:
+    def fly(self):
+        print("Airplane flying")
+
+class Whale:
+    def fly(self):
+        print("Whale swimming")
+
+def lift_off(entity):
+    entity.fly()
+
+parrot = Parrot()
+airplane = Airplane()
+whale = Whale()
+
+lift_off(parrot) # prints `Parrot flying`
+lift_off(airplane) # prints `Airplane flying`
+lift_off(whale) # Throws the error `'Whale' object has no attribute 'fly'`
