84b50e1bf9e25a0c0e38efd4e0816ef3ceb12291
thinkinroad <fengliuzhuan0329@126.com>
2018-01-31 13:07:33 +0800

    1

--- test_openxsl.py
+++ test_openxsl.py
@@ -1,14 +1,23 @@
-from openpyxl import *
-def get_url():
-    url_=[]
-    workbook_ = load_workbook(filename='1.xlsx')
-    sheetnames =workbook_.get_sheet_names()
-    print(sheetnames)
-    sheet = workbook_.get_sheet_by_name(sheetnames[0])
+from openpyxl import Workbook
+from openpyxl.compat import range
+from openpyxl.utils import get_column_letter
 
-    for rowNum in range(1,24):
-        url_.append(sheet.cell(row=rowNum,column=3).value)
-    return url_
-if __name__ == '__main__':
-    url_ = get_url()
-    print(url_)
+wb = Workbook()
+
+dest_filename = 'empty_book.xlsx'
+
+ws1 = wb.active
+ws1.title = "range names"
+
+for row in range(1, 40):
+    ws1.append(range(600))
+ws2 = wb.create_sheet(title="Pi")
+ws2['F5'] = 3.14
+ws3 = wb.create_sheet(title="Data")
+for row in range(10, 20):
+    for col in range(27, 54):
+         _ = ws3.cell(column=col, row=row, value="{0}".format(get_column_letter(col)))
+ws1.merge_cells('A2:D2')
+ws1.unmerge_cells('A2:D2')
+print(ws3['AA10'].value)
+wb.save(filename = dest_filename)
